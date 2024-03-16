53e64facf224afa00b76c01ab07ce792f6880f8e
James Holmes <j.r.holmes@outlook.com>
2024-03-16 11:50:14 -0400

    post class and added gitignore

--- models/post.py
+++ models/post.py
@@ -1,2 +1,12 @@
+from datetime import datetime as dt
+
 class Post:
-    def __init__(self, header : str, date_create : str, recipe : str, instructions : str, description)
+    def __init__(self, header : str, author : str, date_created : dt, date_updated : dt, recipe : str, instructions : str, description : str):
+        self.header = header
+        self.author = author
+        self.date_created = date_created
+        self.date_updated = date_updated
+        self.recipe = recipe
+        self.description = description
+
+    
