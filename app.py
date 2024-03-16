82c71116483d4a4c31b499859202cedcd3fe42eb
James Holmes <j.r.holmes@outlook.com>
2024-03-16 10:17:37 -0400

    Adding the start to the user profile page and templates

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-import re
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -8,19 +7,11 @@ app = Flask(__name__)
 def home():
     return "Hello, Flask!"
 
+@app.route("/hello/")
 @app.route("/hello/<name>")
-def hello_there(name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
-
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-    match_object = re.match("[a-zA-Z]+", name)
-
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = "Hello there, " + clean_name + "! It's " + formatted_now
-    return content
+def hello_there(name = None):
+    return render_template(
+        "user_profile_page.html",
+        name=name,
+        date=datetime.now()
+    )
